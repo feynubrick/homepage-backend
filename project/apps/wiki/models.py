6a2727e30734600212af29af1282e43dd7d49729
Seung-Yoon Oh <feynubrick@proton.me>
2023-03-24 23:07:25 +0900

    simplify Article model

--- project/apps/wiki/models.py
+++ project/apps/wiki/models.py
@@ -4,15 +4,6 @@ from project.commons.models import BaseTimestampedModel
 
 class Article(BaseTimestampedModel):
     deleted = models.BooleanField(help_text="flag for delete", default=False)
-    parent = models.ForeignKey("self", related_name="children", on_delete=models.CASCADE, null=True, blank=True)
-    slug = models.TextField(max_length=200, help_text="Article ID for URL")
-    head_revision = models.ForeignKey(
-        "ArticleRevision", on_delete=models.SET_NULL, related_name="article_as_head", null=True, blank=True
-    )
-
-
-class ArticleRevision(BaseTimestampedModel):
-    article = models.ForeignKey("Article", on_delete=models.CASCADE, related_name="revisions")
     title = models.TextField(max_length=100, help_text="Title", db_index=True)
     body = models.TextField(help_text="Article body")
-    deleted = models.BooleanField(help_text="Flag for delete", db_index=True)
+    slug = models.TextField(max_length=200, help_text="Article ID for URL", db_index=True)
