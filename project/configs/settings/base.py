913e03ebaa2623396dd373c012ee6837e5b5a202
오승윤 <feynubrick@proton.me>
2023-03-26 18:53:08 +0900

    replace sqlite db to postgresql db

--- project/configs/settings/base.py
+++ project/configs/settings/base.py
@@ -82,10 +82,7 @@ WSGI_APPLICATION = "project.configs.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": env.db("DJANGO_DATABASE_URL")
 }
 
 
