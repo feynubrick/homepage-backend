39778af1979a0166eed3fd35dd9256a51573b4cc
Seung-Yoon Oh <feynubrick@proton.me>
2023-04-02 17:06:05 +0900

    added and configured drf_yasg for api docs

--- project/configs/settings/base.py
+++ project/configs/settings/base.py
@@ -35,6 +35,7 @@ DJANGO_APPS = [
 ]
 THIRD_PARTY_APPS = [
     "rest_framework",
+    "drf_yasg",
 ]
 LOCAL_APPS = [
     "project.apps.wiki",
@@ -121,16 +122,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-REST_FRAMEWORK = {
-    # YOUR SETTINGS
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-}
-
-SPECTACULAR_SETTINGS = {
-    'TITLE': 'Your Project API',
-    'DESCRIPTION': 'Your project description',
-    'VERSION': '1.0.0',
-    'SERVE_INCLUDE_SCHEMA': False,
-    # OTHER SETTINGS
-}
--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
-from rest_framework import routers
+from rest_framework import routers, permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from project.apis.wiki.article_views import ArticleViewSet
 
@@ -9,7 +11,22 @@ from project.apis.wiki.article_views import ArticleViewSet
 router = routers.DefaultRouter()
 router.register(r"articles", ArticleViewSet)
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path("api/", include(router.urls)),
     path("admin/", admin.site.urls),
     path("wiki/", include("project.apps.wiki.urls", namespace="wiki")),
