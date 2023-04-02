1be612e29bc4eed386183b2691d50fb29c4f88bf
Seung-Yoon Oh <feynubrick@proton.me>
2023-04-02 16:25:37 +0900

    Revert "add tailwindcss"
    This reverts commit dc00eac1db1024a54ade81fc2368e4edd2af2c50.

--- project/configs/settings/base.py
+++ project/configs/settings/base.py
@@ -33,15 +33,11 @@ DJANGO_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
-THIRD_PARTY_APPS = [
-    "tailwind",
-    "django_browser_reload",
-]
+THIRD_PARTY_APPS = []
 LOCAL_APPS = [
     "project.apps.wiki",
-    "project.apps.theme",
 ]
-INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -51,7 +47,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "project.urls"
@@ -124,9 +119,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-TAILWIND_APP_NAME = "project.apps.theme"
-
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
