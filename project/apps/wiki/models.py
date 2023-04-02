e8166949298192960ef5246e9a2deff675789fd8
Seung-Yoon Oh <feynubrick@proton.me>
2023-04-02 17:21:05 +0900

    clean up SSR parts

--- project/apps/wiki/models.py
+++ project/apps/wiki/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.urls import reverse
 
 from project.commons.models import BaseTimestampedModel
 
@@ -8,7 +7,4 @@ class Article(BaseTimestampedModel):
     deleted = models.BooleanField(help_text="flag for delete", default=False)
     title = models.TextField(max_length=100, help_text="Title", db_index=True)
     body = models.TextField(help_text="Article body")
-    slug = models.TextField(max_length=200, help_text="Article ID for URL", db_index=True)
-
-    def get_absolute_url(self):
-        return reverse("wiki:article_detail", kwargs={"slug": self.slug})
+    slug = models.TextField(max_length=200, help_text="Article ID for URL", db_index=True, unique=True)
--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path, include, re_path
 
 from rest_framework import routers, permissions
@@ -13,21 +12,18 @@ router.register(r"articles", ArticleViewSet)
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      title="Homepage API",
+      default_version="v1",
+      description="API for homepage project",
+      contact=openapi.Contact(email="feynubrick@proton.me"),
+      license=openapi.License(name="MIT"),
    ),
    public=True,
    permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path("api/", include(router.urls)),
-    path("admin/", admin.site.urls),
-    path("wiki/", include("project.apps.wiki.urls", namespace="wiki")),
+    re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    re_path(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("", include(router.urls)),
 ]
